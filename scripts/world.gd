02f3998b97ba506650fcb0415745ac884e13fc3e
analuisa714 <analuisaoliveira714@gmail.com>
2023-12-29 14:23:31 -0300

    primeiro commit

---
+++ scripts/world.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	change_scene()
+
+
+func _on_cliffside_transition_point_body_entered(body):
+	if body.has_method("player"):
+		global.transition_scene = true
+
+
+func _on_cliffside_transition_point_body_exited(body):
+	if body.has_method("player"):
+		global.transition_scene = false
+
+func change_scene():
+	if global.transition_scene == true:
+		if global.current_scene == "world":
+			get_tree().change_scene_to_file("res://scenes/cliff_side.tscn")
+			global.finish_changescenes()
